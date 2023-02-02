6ab7300eddc0f2c2b501217e453341a2be00829b
shu-ting-jia <123926152+shu-ting-jia@users.noreply.github.com>
2023-02-03 01:26:26 +0800

    Update preprocess_wmt14_en_de.sh

--- MT/Fairseq/examples/translation/preprocess_wmt14_en_de.sh
+++ MT/Fairseq/examples/translation/preprocess_wmt14_en_de.sh
@@ -6,7 +6,7 @@ TEXT=examples/translation/wmt14_en_de
 
 fairseq-preprocess \
     --source-lang en --target-lang de \
-    --trainpref $TEXT/train_short --validpref $TEXT/valid --testpref $TEXT/outtest \
+    --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir data-bin/wmt14_en_de --thresholdtgt 0 --thresholdsrc 0 \
     --nwordssrc -1 --nwordstgt -1 \
     --joined-dictionary --workers 20
