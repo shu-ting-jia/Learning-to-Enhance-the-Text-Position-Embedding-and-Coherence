0c54b80db4c35a7f4ee13eedb0438a6ba0fbc0c3
shu-ting-jia <123926152+shu-ting-jia@users.noreply.github.com>
2023-02-03 01:25:56 +0800

    Update preprocess_iwslt14_de_en.sh

--- MT/Fairseq/examples/translation/preprocess_iwslt14_de_en.sh
+++ MT/Fairseq/examples/translation/preprocess_iwslt14_de_en.sh
@@ -6,6 +6,6 @@ TEXT=examples/translation/iwslt14.tokenized.de-en
 
 fairseq-preprocess \
     --source-lang de --target-lang en \
-    --trainpref $TEXT/trainoutput --validpref $TEXT/valid --testpref $TEXT/outtest \
+    --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir data-bin/iwslt14.tokenized.de-en \
     --workers 20
